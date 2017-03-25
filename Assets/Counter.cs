c8ba359822c75b9411e92cbe37523f033d19db59
Colin Daly <dalyco884@gmail.com>
2017-03-25 09:33:30 -0400

    level 1 now works

---
+++ Assets/Counter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour {
+
+	private MainController mc;
+	private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		mc = GameObject.Find ("mainController").GetComponent<MainController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "Tomato") {
+			print ("tomato!");
+			score++;
+		} else if (col.gameObject.name == "Cheese") {
+			print ("cheese!");
+			score++;
+		} else if (col.gameObject.name == "Lettuce") {
+			print ("lettuce!");
+			score++;
+		}
+
+		if (score >= 3) {
+			mc.goToLevel (2);
+		}
+	}
+}
---
+++ Assets/MainController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goToLevel(int level){
+		if (level == 2) {
+			//setup level 2 of cooking burger
+			//for now just change text
+			GameObject.Find("instructions").GetComponent<Text>().text = "Nice!";
+		}
+	}
+}
